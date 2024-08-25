56345264e39743cf6f80492744e08a6d25591962
Karim CHARLEUX <karim.charleux@hotmail.fr>
2024-08-25 23:16:08 +0200

    Init the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'web-enzo-coaching';
+  constructor(private titleService: Title) {
+    this.titleService.setTitle("Enzo CAVANNA - Coach sportif");
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {AuthGuard} from "./core/authGuard";
+import {HomeComponent} from "./front-office/pages/home/home.component";
+import {DashboardComponent} from "./back-office/pages/dashboard/dashboard.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'admin',
+    component: DashboardComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
